34b1ec20c86044c0e00c26a0b8d88dae4ea257e1
Nord-6925 <321745443+ntijoh-tage-walder@users.noreply.github.com>
2026-09-25 14:32:30 +0200

    Store and load messeges from memory
    Stores messages in est tabel in memory and then loads on restart or connect

--- lib/zelestia_web/live/chat.ex
+++ lib/zelestia_web/live/chat.ex
@@ -7,22 +7,36 @@ defmodule ZelestiaWeb.Chat do
 
   def render(assigns) do
     ~H"""
-    <LiveSvelte.svelte name="ChatRoom" props={%{messages: @messages}}/>
+    <LiveSvelte.svelte name="ChatRoom" props={%{messages: @messages}} />
     """
   end
 
   def mount(_params, _session, socket) do
     ZelestiaWeb.Endpoint.subscribe(@topic)
-     {:ok, assign(socket, messages: [])}
+
+    messages =
+      :ets.tab2list(:messages)
+      |> format([])
+
+    {:ok, assign(socket, messages: messages)}
+  end
+
+  defp format([], acc), do: Enum.reverse(acc)
+  defp format([head | tail], acc) do
+    {id, name, body} = head
+    format(tail, [%{id: id, name: name, body: body} | acc])
   end
 
   def handle_event("send_message", event, socket) do
+    size = :ets.info(:messages, :size)
+    event = Map.put(event, "id", size + 1)
+    :ets.insert(:messages, {event["id"] ,event["name"], event["body"]})
     ZelestiaWeb.Endpoint.broadcast(@topic, @event_new_message, event)
     {:noreply, socket}
   end
 
   def handle_info(%{topic: @topic, event: @event_new_message, payload: payload}, socket) do
-    payload = Map.put(payload, :id, System.unique_integer([:positive]))
+    #payload = Map.put(payload, :id, System.unique_integer([:positive]))
     {:noreply, assign(socket, messages: socket.assigns.messages ++ [payload])}
   end
 
